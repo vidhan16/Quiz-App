7acd76f41c57078faf7015c723cf896acd40952c
vidhan <vidhan@vidhan.vidhan>
2024-09-02 00:50:54 +0530

    Landing Page(Responsive)

--- frontend/index.js
+++ frontend/index.js
@@ -4,3 +4,19 @@ var typed = new Typed(".auto-type", {
     backSpeed : 120,
     loop : true
 })
+
+var option_button = document.querySelector('.right > :nth-child(2)');
+var more_options = document.querySelector('.more-options');
+console.log(more_options);
+
+let toggler = 0;
+option_button.addEventListener('click',()=>{
+    if(!toggler){
+        more_options.style.display="flex";
+        toggler = 1;
+    }
+    else{
+        more_options.style.display="none";
+        toggler = 0;
+    }
+})
