d9aaae97e2a8b618f7f8d253885dd638accb6f48
vidhan <vidhan@vidhan.vidhan>
2024-09-07 15:52:26 +0530

    Quiz Page added

--- frontend/index.js
+++ frontend/index.js
@@ -21,3 +21,30 @@ option_button.addEventListener('click',()=>{
         toggler = 0;
     }
 })
+
+let catArr = [18,19,30,17,22,23,31,27,10,24,25,11];
+let topics = document.querySelectorAll('.topics');
+let topic = document.querySelectorAll('.head3');
+
+let display_question = document.querySelector('.question-title');
+let currQuestion = 0;
+for(let i = 0 ; i < topics.length ; i++)
+{
+    topics[i].addEventListener('click',()=>{
+        localStorage.setItem('category',catArr[i]);
+        window.location.href = './frontend/quiz.html';
+        // fetch(`https://opentdb.com/api.php?amount=5&category=${catArr[i]}&difficulty=medium&type=multiple`)
+        // .then(response => {
+        //     if (!response.ok) {
+        //       throw new Error('Network response was not ok ' + response.statusText);
+        //     }
+        //     return response.json();
+        //   })
+        //   .then(data => {
+        //     display_question.textContent=`${data.results[currQuestion].question}`;
+        //   })
+        //   .catch(error => {
+        //     console.error('There has been a problem with your fetch operation:', error);
+        //   });
+    })
+}
---
+++ frontend/quiz.js
@@ -0,0 +1,88 @@
+let categoryCount = localStorage.getItem('category');
+let correctAnswer = "";
+let questionIndex = 0;
+let questions = [];
+
+async function loadQuestions()
+{
+    const url = `https://opentdb.com/api.php?amount=5&category=${categoryCount}&type=multiple`;
+    const result = await fetch(url);
+    const data = await result.json();
+    questions = data.results;
+    showQuestion();
+}
+
+let display_question = document.querySelector('.question-title');
+let option1 = document.querySelector('.option-1');
+let option2 = document.querySelector('.option-2');
+let option3 = document.querySelector('.option-3');
+let option4 = document.querySelector('.option-4');
+let nextButton = document.querySelector('.next');
+let prevButton = document.querySelector('.prev');
+nextButton.addEventListener('click',()=>{
+    next();
+})
+prevButton.addEventListener('click',()=>{
+    prev();
+})
+
+let optionsArr = [option1,option2,option3,option4];
+function showQuestion()
+{
+    display_question.textContent = `${questions[questionIndex].question}`;
+    correctAnswer = `${questions[questionIndex].correct_answer}`;
+    let incorrectAnswers = questions[questionIndex].incorrect_answers;
+    let randomno = Math.floor(Math.random() * 4);
+    optionsArr[randomno].innerHTML = correctAnswer + optionsArr[randomno].innerHTML;
+    let it = 0;
+    for(let i = 0 ; i < 4 ; i++)
+    {
+        if(i != randomno)
+        {
+            optionsArr[i].innerHTML = incorrectAnswers[it++] + optionsArr[i].innerHTML;
+        }
+    }
+}
+function next()
+{
+    if(questionIndex == questions.length-1)
+    {
+        return;
+    }
+    for(let i = 0 ; i < 4 ; i++)
+    {
+        optionsArr[i].innerHTML=
+        `<div class="tick-mark">
+            <img width="24" height="24" src="https://img.icons8.com/material-sharp/24/40C057/checked--v1.png" alt="checked--v1"/>
+        </div>`
+    }
+    questionIndex++;
+    showQuestion();
+}
+function prev()
+{
+    if(questionIndex == 0)
+    {
+        return;
+    }
+    for(let i = 0 ; i < 4 ; i++)
+    {
+        optionsArr[i].innerHTML=
+        `<div class="tick-mark">
+            <img width="24" height="24" src="https://img.icons8.com/material-sharp/24/40C057/checked--v1.png" alt="checked--v1"/>
+        </div>`
+    }
+    questionIndex--;
+    showQuestion();
+}
+loadQuestions();
+
+let optionClick = document.querySelectorAll('.options > div');
+let tickMark = document.querySelectorAll('.tick-mark');
+for(let i = 0 ; i < optionClick.length ; i++)
+{
+    optionClick[i].addEventListener('click',()=>{
+        console.log(tickMark[i]);
+        tickMark[i].style.visibility = "visible";
+    })
+}
