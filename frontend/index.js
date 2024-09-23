1ecd88b3d735cd9b5c330e63e067d980de1b1201
vidhan <vidhan@vidhan.vidhan>
2024-09-23 15:24:39 +0530

    Login Page Modified

--- frontend/index.js
+++ frontend/index.js
@@ -22,6 +22,15 @@ option_button.addEventListener('click',()=>{
     }
 })
 
+let more_options_links = document.querySelectorAll('.more-options a');
+for(let i = 0 ; i < more_options_links.length ; i++)
+{
+    more_options_links[i].addEventListener('click',()=>{
+        more_options.style.display="none";
+        toggler = 0;
+    })
+}
+
 let catArr = [18,19,30,17,22,23,31,27,10,24,25,11];
 let topics = document.querySelectorAll('.topics');
 let topic = document.querySelectorAll('.head3');
@@ -48,10 +57,12 @@ sliderBtn.addEventListener('click',()=>{
     if(!sliderToggle)
     {
         slidingWindow.style.transform = "translate(100%, 0)";
+        sliderBtn.textContent="SIGNUP";
         sliderToggle = 1;
     }
     else{
         slidingWindow.style.transform = "translate(0, 0)";
+        sliderBtn.textContent="LOGIN";
         sliderToggle = 0;
     }
 })
@@ -69,4 +80,82 @@ let closeButton = document.querySelector('.cross-button');
 closeButton.addEventListener('click',()=>{
     loginWindow.style.display = "none";
     document.querySelector('body').style.overflow="visible";
+})
+
+let alreadyBtn = document.querySelector('.already_login');
+let loginSide = document.querySelector('.login_side');
+let signupSide = document.querySelector('.signup_side');
+alreadyBtn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    signupSide.style.display="none";
+    loginSide.style.display="block";
+})
+
+let notLogged = document.querySelector('.not_logged span');
+notLogged.addEventListener('click',(e)=>{
+    e.preventDefault();
+    signupSide.style.display="block";
+    loginSide.style.display="none";
+})
+
+let loginAuth = document.querySelector('.login-btn');
+let signupAuth = document.querySelector('.signup-btn');
+
+function register_user(emailInput, userInput, passInput) {
+    let oldUser = JSON.parse(localStorage.getItem('users')) || [];
+    
+    let user = {
+        username: userInput,
+        emailid: emailInput,
+        password: passInput
+    };
+
+    oldUser.push(user);
+    
+    localStorage.setItem('users', JSON.stringify(oldUser));
+}
+
+
+signupAuth.addEventListener('click',(e)=>{
+    e.preventDefault();
+    let emailInput = document.querySelector('.emailid_signup').value;
+    let userInput = document.querySelector('.username_signup').value;
+    let passInput = document.querySelector('.password_signup').value;
+    if(emailInput!=""  && userInput!="" && passInput != "" )
+    {
+        register_user(emailInput,userInput,passInput);
+    }
+})
+
+
+function authenticate_user(userInput,passInput)
+{
+    let storedUsers = JSON.parse(localStorage.getItem('users'));
+    for(let i = 0 ; i < storedUsers.length ; i++)
+    {
+        console.log(userInput);
+        console.log(storedUsers[i].username);
+        console.log(passInput);
+        console.log(storedUsers[i].password);
+        if(storedUsers[i].username == userInput && storedUsers[i].password == passInput)
+        {
+            loginWindow.style.display="none";
+            document.querySelector('.auth-success').style.display="block";
+            document.querySelector('.login-button').textContent=`Welcome, ${userInput}`;
+            setTimeout(() => {
+                document.querySelector('.auth-success').style.display="none";
+            }, 3000);
+            return;
+        }
+    }
+    console.log("Incorrect");
+}
+
+loginAuth.addEventListener('click',()=>{
+    let userInput = document.querySelector('.username_login').value;
+    let passInput = document.querySelector('.password_login').value;
+    if(userInput!=""  && passInput != "")
+    {
+        authenticate_user(userInput,passInput);
+    }    
 })
